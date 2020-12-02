2b413c2c56043197dea024e5b349be701e19822e
pengwenchao <564845017@qq.com>
2020-12-02 16:23:47 +0800

    test

--- src/test/java/com/self/study/StudyApplicationTests.java
+++ src/test/java/com/self/study/StudyApplicationTests.java
@@ -1,17 +1,17 @@
-package com.self.study;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-/**
- * 不加RunWith会出现Dao层无法注入的问题
- */
-@SpringBootTest
-//@RunWith(SpringJUnit4ClassRunner.class)
-class StudyApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.self.study;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+///**
+// * 不加RunWith会出现Dao层无法注入的问题
+// */
+//@SpringBootTest
+////@RunWith(SpringJUnit4ClassRunner.class)
+//public class StudyApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
