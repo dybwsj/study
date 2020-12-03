bc9a4b17a06b884fe3a6b9c0e63ac935b646dc17
pengwenchao <564845017@qq.com>
2020-12-03 17:13:50 +0800

    test

--- src/main/java/com/self/study/StudyApplication.java
+++ src/main/java/com/self/study/StudyApplication.java
@@ -7,7 +7,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class StudyApplication {
-    String s = "s";
     public static void main(String[] args) {
         SpringApplication.run(StudyApplication.class, args);
     }
